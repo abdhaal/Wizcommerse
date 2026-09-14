65c5827c5944afd47c84f6b867f3a39814c4a349
abdhaal <abdhaal0@gmail.com>
2026-09-15 01:44:10 +0530

    Fix formatting issue in product-details.js

--- js/product-details.js
+++ js/product-details.js
@@ -25,7 +25,7 @@ const productDatabase = {
         seller: "Wiz Verified Store",
         sellerTrustScore: 96,
         customerPhotos: 48,
-        image: "assets/images/bulb.png"
+        image: "assets/images/bulb.png",
         deliveryDate: "18 Sep 2026"
     },
 
